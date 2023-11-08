c73af4a6defbb138feb5bf932335e0db4ba1879b
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-11-08 15:31:02 +0100

    adding Header component

---
+++ src/view/components/Header.jsx
@@ -0,0 +1,41 @@
+function Header() {
+    return (
+        <>
+            <header>
+                <h1>Bruh</h1>
+                <img src="" alt="logo" />
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="">Home
+                                <img src="" alt="" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">Profil
+                                <img src="" alt="" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">Transaction
+                                <img src="" alt="" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">Chart
+                                <img src="" alt="" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">Disconnect
+                                <img src="" alt="" />
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
+
+export default Header;
