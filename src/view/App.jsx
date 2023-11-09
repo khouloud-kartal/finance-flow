9f4a63d8d48e859571b31a58c6bdcbf84b614144
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-11-09 15:33:12 +0100

    way of display changeg in App.jsx with useState

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -1,42 +1,25 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './assets/App.css'
-import Header from "./components/Header"
-import TopBar from "./components/TopBar"
-import Balance from "./components/Balance"
-import CategoriesDisplay from "./components/CategoriesDisplay"
-import AddTransaction from "./components/AddTrasaction"
-import FourLastestTransaction from "./components/FourLastestTransaction"
+// import Header from "./components/Header"
+// import TopBar from "./components/TopBar"
+// import Balance from "./components/Balance"
+// import CategoriesDisplay from "./components/CategoriesDisplay"
+// import AddTransaction from "./components/AddTrasaction"
+// import FourLastestTransaction from "./components/FourLastestTransaction"
+import Index from "./components/pages"
+
+let index = <Index />
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [page, setPage] = useState(index);
 
   return (
     <>
-      <Header />
-        <main>
-          <section>            
-              <TopBar />
-              <div>
-                <div className="container-transaction">
-                  <AddTransaction />
-                  <FourLastestTransaction />
-                </div>
-
-                <aside>
-                  <div>
-                    <Balance />
-                  </div>
-                  <div>
-                    <CategoriesDisplay />
-                  </div>
-                </aside>
-              </div>
-          </section>
-        </main>
+      {page}
     </>
-  )
+  );
 }
 
 export default App
